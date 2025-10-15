8d2edc083b2aa03cdcf66aac2b404cd22e06d58b
THE1st <jdos46juan@gmail.com>
2025-10-15 09:12:36 -0500

    Correciones menores

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { Request, Response } from "express";
 import app from "./app";
 import "dotenv/config";
 
+const PORT: string | number = process.env["PORT"] || 3000;
+
 app.get("/", (_req: Request, res: Response): void => {
-	res.json({ message: "API activa 🚀" });
+	res.send("API activa 🚀");
+});
+
+app.listen(PORT, (): void => {
+	console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
