efa17c3c3001061a4d1332e3aff73c5792e0e27e
THE1st <jdos46juan@gmail.com>
2025-10-15 21:47:16 -0500

    pruebas

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import app from "./app";
 import "dotenv/config";
 
-// const PORT: number = parseInt(process.env["PORT"] as string, 10) || 3000;
+const PORT: number = parseInt(process.env["PORT"] as string, 10) || 3000;
 
 app.get("/", (_req: Request, res: Response): void => {
 	res.json({ message: "API activa 🚀" });
 });
 
-// app.listen(PORT, (): void => {
-// 	console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
+app.listen(PORT, (): void => {
+	console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
 export default app;
